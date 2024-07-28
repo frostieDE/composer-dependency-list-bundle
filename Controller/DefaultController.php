eb5ab5c4358f89b30883f4ffa881b8e8dce3ae01
Marcel Marnitz <marcelmarnitz@googlemail.com>
2024-07-28 14:54:49 +0200

    Use attribute instead of annotation (resolved deprecation error)

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,7 +7,7 @@ use FrostieDE\ComposerDependencyList\Dependency;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController {
 
